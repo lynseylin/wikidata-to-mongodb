1bb266e1fbe72ee3dda1c19dc66c2d7f85c75789
lynseylin <lynseylin@qq.com>
2018-01-30 11:37:40 +0800

    Update config.php

--- config.php
+++ config.php
@@ -7,6 +7,6 @@
  */
 
 define('MEMORY_LIMIT', '10240M');
-define('BULK_COUNT', 10000);
+define('BULK_COUNT', 1000);
 define('JSON_FILE', "/kjd/20170612.json");
 define('MONGODB_SERVER', "mongodb://127.0.0.1:27017");
